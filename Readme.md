d0fc7d5d7505d25b388eb3dece3fe54bfbd1fdd3
Ben Gourley <bn@grly.me>
2012-12-03 07:45:32 +0000

    More tests. Write readme.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,36 @@
 # Slugg
 
 Make strings url-safe.
+
+- Comprehensive tests
+- No dependencies
+- Not in coffee-script (lol)
+- Coerces foreign symbols to their english equivalent
+- Doesn't try to do anything fancy with symbols (just removes them)
+
+```
+npm install slugg
+```
+
+## Usage:
+
+### slug(string, [separator])
+
+```js
+var slug = require('slugg')
+
+slug('My fantastic blog post')
+//-> 'my-fantastic-blog-post'
+
+slug('Today I found £5')
+//-> 'today-i-found-5'
+
+slug('I ♥ you')
+//-> 'i-you'
+```
+
+If you want a separator other than '-', pass it in as the second argument:
+
+```js
+slug('Kevin Spacey', ' ')
+//-> 'kevin spacey'
--- test/slugg.test.js
+++ test/slugg.test.js
@@ -1,37 +1,54 @@
-var slugg = require('..')
+var slug = require('..')
   , assert = require('assert')
 
 describe('slug()', function () {
 
   it('should not modify a url-safe string', function () {
-    assert.equal(slugg('latest-news'), 'latest-news')
-    assert.equal(slugg('-'), '')
+    assert.equal(slug('latest-news'), 'latest-news')
+    assert.equal(slug('-'), '')
   })
 
   it('should convert non-word characters to dashes', function () {
-    assert.equal(slugg('my great blog post'), 'my-great-blog-post')
-    assert.equal(slugg('who.are_you?'), 'who-are-you')
-    assert.equal(slugg('%^T%^~!@##$$#%^$^/?????.'), 't')
+    assert.equal(slug('my great blog post'), 'my-great-blog-post')
+    assert.equal(slug('who.are_you?'), 'who-are-you')
+    assert.equal(slug('%^T%^~!@##$$#%^$^/?????.'), 't')
   })
 
   it('should replace non-english alphanumeric characters', function () {
-    assert.equal(slugg('Füße'), 'fusse')
-    assert.equal(slugg('lýs'), 'lys')
-    assert.equal(slugg('£5+5'), '5-5')
+    assert.equal(slug('Füße'), 'fusse')
+    assert.equal(slug('lýs'), 'lys')
+    assert.equal(slug('£5+5'), '5-5')
   })
 
   it('should not produce a string with successive dashes', function () {
-    assert.equal(slugg('this cost £50'), 'this-cost-50')
-    assert.equal(slugg('eh----what'), 'eh-what')
-    assert.equal(slugg('Foo & Bar Bazing Day:'), 'foo-bar-bazing-day')
+    assert.equal(slug('this cost £50'), 'this-cost-50')
+    assert.equal(slug('eh----what'), 'eh-what')
+    assert.equal(slug('Foo & Bar Bazing Day:'), 'foo-bar-bazing-day')
   })
 
   it('should not produce a string with leading/trailing dashes', function () {
-    assert.equal(slugg('----boo!--'), 'boo')
+    assert.equal(slug('----boo!--'), 'boo')
   })
 
   it('should convert to lower case', function () {
-    assert.equal(slugg('HEY ThErE'), 'hey-there')
+    assert.equal(slug('HEY ThErE'), 'hey-there')
+  })
+
+  it('should use a custom separator if present', function () {
+    assert.equal(slug('I ♥ you', ' '), 'i you')
+  })
+
+  it('should convert letter-like chars into english alphanumeric chars', function () {
+    Object.keys(slug.chars).forEach(function (key) {
+      assert(/\w+/.test(slug(key)), key + slug(key))
+    })
+  })
+
+  it('should do what it says in the readme', function () {
+    assert.equal(slug('My fantastic blog post'), 'my-fantastic-blog-post')
+    assert.equal(slug('Today I found £5'), 'today-i-found-5')
+    assert.equal(slug('I ♥ you'), 'i-you')
+    assert.equal(slug('Kevin Spacey', ' '), 'kevin spacey')
   })
 
 })
